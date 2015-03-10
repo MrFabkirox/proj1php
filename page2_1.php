950879ac8b425aafd3dc068cafae2af3be1956e8
fabrice <fabrice.esope@gmail.com>
2015-03-10 12:11:24 +0100

    form

--- page2_1.php
+++ page2_1.php
@@ -11,4 +11,15 @@
 		<li><a href="/proj1/page3_1.php">no parametre</a></li>
 	</ul>
 
+	<form action="" type="GET">
+		<input type="text" placeholder="Some text" name="tosend"></input>
+		<input type="submit" value="Send to this page">
+	</form>
+
 </p>
+
+<?php
+	
+	echo "Value of the form: ".$_GET['tosend'];
+
+?>
